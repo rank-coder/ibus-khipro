6a292a814a7291459b8960c919cd60c77af4a115
rank_coder <54497225+rank-coder@users.noreply.github.com>
2024-06-19 21:42:15 +0600

    Update avrolib.js

--- avrolib.js
+++ avrolib.js
@@ -751,26 +751,6 @@ OmicronLab.Avro.Phonetic = {
     ]
   },
   {
-    "find": "nff",
-    "replace": "ঞ্",
-    "rules": [
-      {
-        "replace": "ঞ",
-        "matches": [
-          {
-            "type": "suffix",
-            "scope": "!consonant"
-          },
-          {
-            "type": "suffix",
-            "scope": "!exact",
-            "value": "`"
-          }
-        ]
-      }
-    ]
-  },
-  {
     "find": "nf",
     "replace": "ণ্",
     "rules": [
@@ -796,6 +776,26 @@ OmicronLab.Avro.Phonetic = {
     "rules": [
       {
         "replace": "ন",
+        "matches": [
+          {
+            "type": "suffix",
+            "scope": "!consonant"
+          },
+          {
+            "type": "suffix",
+            "scope": "!exact",
+            "value": "`"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "find": "mf",
+    "replace": "ঞ্",
+    "rules": [
+      {
+        "replace": "ঞ",
         "matches": [
           {
             "type": "suffix",
